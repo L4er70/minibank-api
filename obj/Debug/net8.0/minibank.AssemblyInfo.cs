c957e8d1bcda7c415384d02d509232e8837c0392
L4er70 <laert.juti11@gmail.com>
2026-03-10 10:13:47 +0100

    DbContext added

--- obj/Debug/net8.0/minibank.AssemblyInfo.cs
+++ obj/Debug/net8.0/minibank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minibank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1a6029a7b1a4aaecea94824aac5a78607ddbab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c278d34dcd15eec0cc3e71f2407a4d9e5fe5f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("minibank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minibank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
